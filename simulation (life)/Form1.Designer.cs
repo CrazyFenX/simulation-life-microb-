2a916cb949408b47994906bba1c30bad546a71a1
Кирилл <kattoshkin@gmail.com>
2021-07-30 22:51:22 +0700

    Добавьте файлы проекта.

---
+++ simulation (life)/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace simulation__life_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerOfMove = new System.Windows.Forms.Timer(this.components);
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timerDrawing = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timerOfMove
+            // 
+            this.timerOfMove.Tick += new System.EventHandler(this.timerOfMove_Tick);
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(895, 599);
+            this.startButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(74, 36);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Marlett", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(907, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.stopButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.Location = new System.Drawing.Point(895, 644);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(74, 36);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "stop";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(895, 413);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "rnd";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timerDrawing
+            // 
+            this.timerDrawing.Tick += new System.EventHandler(this.timerDrawing_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(895, 453);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "clr";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1016, 691);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Timer timerOfMove;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timerDrawing;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ simulation (life)/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simulation__life_
+{
+    public partial class Form1 : Form
+    {
+        private ushort[,] cells;
+        private ushort[,] templateCells;
+        Graphics graph;
+
+        bool isitdown = false;
+        bool isItEraser = false;
+        Size cellSize = new Size(5, 5);
+        Point location = new Point(0, 0);
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            cells = new ushort[100, 100];
+            templateCells = new ushort[100, 100];
+            cellSize = new Size(7, 7);
+            location = new Point(0, 0);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            graph = e.Graphics;
+            DrawIt();
+        }
+
+        protected void DrawIt()
+        {
+            for (int i = 0; i < cells.GetLength(0); i++)
+                for (int j = 0; j < cells.GetLength(1); j++)
+                {
+                    var rect = new Rectangle(location.X + i * cellSize.Width, location.Y + j * cellSize.Height, cellSize.Width, cellSize.Height);
+                    var brush = cells[i, j] == (byte)1 ? Brushes.Black : Brushes.White;
+                    graph.FillRectangle(brush, rect);
+                    //graph.DrawRectangle(Pens.Black, rect);
+                }
+        }
+
+        protected void drawTheNet()
+        {
+            for (int i = 0; i < cells.GetLength(0); i++)
+                for (int j = 0; j < cells.GetLength(1); j++)
+                {
+                    var rect = new Rectangle(location.X + i * cellSize.Width, location.Y + j * cellSize.Height, cellSize.Width, cellSize.Height);
+                    //graph.FillRectangle(Brushes.White, rect);
+                    graph.DrawRectangle(Pens.Black, rect);
+                }
+        }
+
+        private void drawLocalChange(int i, int j)
+        {
+            Rectangle rect = new Rectangle(location.X + i * cellSize.Width, location.Y + j * cellSize.Height, cellSize.Width, cellSize.Height);
+            //Brush brush = cells[i, j] == (byte)1 ? Brushes.Black : Brushes.White;
+            Brush brush;
+            switch (cells[i, j])
+            {
+                case 0:
+                    brush = Brushes.White;
+                    break;
+                case 1:
+                    brush = Brushes.Black;
+                    break;
+                case 2:
+                    brush = Brushes.Red;
+                    break;
+                default:
+                    brush = Brushes.White;
+                    break;
+            }
+            graph.FillRectangle(brush, rect);
+            //graph.DrawRectangle(Pens.Black, rect);
+        }
+
+        private void syncronizeMaps()
+        {
+            for (int i = 1; i < cells.GetLength(0) - 1; i++)
+                for (int j = 1; j < cells.GetLength(1) - 1; j++)
+                {
+                    templateCells[i, j] = cells[i, j];
+                }
+        }
+
+        private void bornPoints ()
+        {
+            ushort sum = 0;
+            ushort[,] tmpCells = new ushort[cells.GetLength(0), cells.GetLength(1)];
+            syncronizeMaps();
+
+            for (int i = 1; i < cells.GetLength(0) - 1; i++)
+                for (int j = 1; j < cells.GetLength(1) - 1; j++)
+                {
+                    sum = getSum(i, j);
+                    tmpCells[i, j] = sum;
+                }
+
+            for (int i = 1; i < cells.GetLength(0) - 1; i++)
+                for (int j = 1; j < cells.GetLength(1) - 1; j++)
+                {
+                    if (tmpCells[i, j] == 3)
+                    {
+                        cells[i, j] = 1;
+                        if (rnd.Next() % 100 == 1 || rnd.Next() % 100 == 2)
+                            cells[i, j] = 2;
+                    }
+                    //else if (tmpCells[i, j] != 2 && tmpCells[i, j] != 3 && tmpCells[i, j] != 4 && tmpCells[i, j] != 5 && tmpCells[i, j] != 6 && tmpCells[i, j] != 7 && tmpCells[i, j] != 8)
+                    else if (tmpCells[i, j] != 2 && tmpCells[i, j] != 3)
+                    {
+                        cells[i, j] = 0;
+                    }
+                    //if (cells[i, j] == 2 && rnd.Next() % 10 >= 9)
+                    if (cells[i, j] == 2 && tmpCells[i, j] > 1)
+                    {
+                        cells[i + 1, j] = 2;
+                        cells[i, j + 1] = 2;
+                        cells[i, j - 1] = 2;
+                        cells[i - 1, j] = 2;
+                        cells[i, j] = 0;
+                    }
+                }
+            drawFromChecking();
+        }
+
+        private void drawFromChecking()
+        {
+            for (int i = 1; i < cells.GetLength(0) - 1; i++)
+                for (int j = 1; j < cells.GetLength(1) - 1; j++)
+                {
+                    if (templateCells[i, j] != cells[i, j])
+                    {
+                        graph = this.CreateGraphics();
+                        drawLocalChange(i, j);
+                    }
+                }
+        }
+
+        private ushort getSum(int i, int j)
+        {
+            ushort sum = 0;
+            if (cells[i - 1, j - 1] == 1)
+                sum++;
+            if (cells[i - 1, j] == 1)
+                sum++;
+            if (cells[i - 1, j + 1] == 1)
+                sum++;
+            if (cells[i, j - 1] == 1)
+                sum++;
+            if (cells[i, j + 1] == 1)
+                sum++;
+            if (cells[i + 1, j - 1] == 1)
+                sum++;
+            if (cells[i + 1, j] == 1)
+                sum++;
+            if (cells[i + 1, j + 1] == 1)
+                sum++;
+            return sum;
+        }
+
+        private void randomPointGeneration()
+        {
+            Random rnd = new Random();
+            for (int i = 1; i < cells.GetLength(0) - 1; i++)
+                for (int j = 1; j < cells.GetLength(1) - 1; j++)
+                {
+                    if (rnd.Next() % 5 == 1)
+                    {
+                        cells[i, j] = 1;
+                        //if (rnd.Next() % 100 == 1)
+                        //    cells[i, j] = 2;
+                    }
+                }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int width = e.X / cellSize.Width;
+            int height = e.Y / cellSize.Height;
+            if (cells.GetLength(0) > width && cells.GetLength(1) > height)
+                if ( e.Button == MouseButtons.Left)
+                {
+                    cells[width, height] = (byte)1;
+                    graph = this.CreateGraphics();
+                    drawLocalChange(width, height);
+                    isitdown = true;
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+                    cells[width, height] = (byte)0;
+                    graph = this.CreateGraphics();
+                    drawLocalChange(width, height);
+                    isItEraser = true;
+                }
+        }
+
+        private void timerOfMove_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+            bornPoints();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            timerOfMove.Start();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timerOfMove.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            randomPointGeneration();
+            drawFromChecking();
+        }
+
+        private void timerDrawing_Tick(object sender, EventArgs e)
+        {
+            //graph = this.CreateGraphics();
+            //drawTheNet();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isitdown = false;
+            isItEraser = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isitdown)
+            {
+                int width = e.X / cellSize.Width;
+                int height = e.Y / cellSize.Height;
+                if (cells.GetLength(0) > width && cells.GetLength(1) > height)
+                {
+                    cells[width, height] = (byte)1;
+                    if (rnd.Next() % 100 == 1 || rnd.Next() % 100 == 2)
+                        cells[width, height] = 2;
+                    graph = this.CreateGraphics();
+                    drawLocalChange(width, height);
+                }
+            }
+            if (isItEraser)
+            {
+                int width = e.X / cellSize.Width;
+                int height = e.Y / cellSize.Height;
+                if (cells.GetLength(0) > width && cells.GetLength(1) > height)
+                {
+                    cells[width, height] = (byte)0;
+                    graph = this.CreateGraphics();
+                    drawLocalChange(width, height);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < cells.GetLength(0) - 1; i++)
+                for (int j = 1; j < cells.GetLength(1) - 1; j++)
+                {
+                    cells[i, j] = 0;
+                    graph = this.CreateGraphics();
+                    drawLocalChange(i, j);
+                }
+        }
+    }
+}
